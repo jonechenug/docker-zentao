43bcb764bd4a6e2d0817de0602dbc618270c4db2
chenug <jonechenug@gmail.com>
2017-02-21 13:27:20 +0800

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
+ZENTAO_VERSION=9.0.1
 set -x
 
 dir=`ls /opt/zbox`
 
 if [ -z "${dir}" ];then
   cd /tmp
-  tar -zxvf  ZenTaoPMS.8.3.1.zbox_64.tar.gz -C   /opt
+  tar -zxvf  ZenTaoPMS.$ZENTAO_VERSION.zbox_64.tar.gz -C   /opt
 fi
 
 /opt/zbox/zbox start && tail -f /dev/null
